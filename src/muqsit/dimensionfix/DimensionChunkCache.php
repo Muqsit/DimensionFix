6237474959bd0a484fa435193d7ba9994cadd3e5
ᴰᵃʳᵏⁿᵉˢˢᴾᴸ <ktostamhip@gmail.com>
2025-01-30 21:48:42 +0100

    Update DimensionChunkCache.php
    Fixing

--- src/muqsit/dimensionfix/DimensionChunkCache.php
+++ src/muqsit/dimensionfix/DimensionChunkCache.php
@@ -12,84 +12,89 @@ use pocketmine\world\World;
 use ReflectionClass;
 use ReflectionProperty;
 
-final class DimensionChunkCache extends ChunkCache{
+final class DimensionChunkCache extends ChunkCache {
 
-	public static function from(ChunkCache $parent, int $dimension_id) : self{
-		static $_world = new ReflectionProperty(ChunkCache::class, "world");
-		static $_caches = new ReflectionProperty(ChunkCache::class, "caches");
-		static $_hits = new ReflectionProperty(ChunkCache::class, "hits");
-		static $_misses = new ReflectionProperty(ChunkCache::class, "misses");
-		static $_compressor = new ReflectionProperty(ChunkCache::class, "compressor");
-		static $_this = new ReflectionClass(self::class);
-		$instance = $_this->newInstanceWithoutConstructor();
-		$instance->dimension_id = $dimension_id;
-		$_world->setValue($instance, $_world->getValue($parent));
-		$_compressor->setValue($instance, $_compressor->getValue($parent));
-		$_caches->setValue($instance, $_caches->getValue($parent));
-		$_hits->setValue($instance, $_hits->getValue($parent));
-		$_misses->setValue($instance, $_misses->getValue($parent));
-		return $instance;
-	}
+    public static function from(ChunkCache $parent, int $dimension_id) : self {
+        static $_world = new ReflectionProperty(ChunkCache::class, "world");
+        static $_caches = new ReflectionProperty(ChunkCache::class, "caches");
+        static $_hits = new ReflectionProperty(ChunkCache::class, "hits");
+        static $_misses = new ReflectionProperty(ChunkCache::class, "misses");
+        static $_compressor = new ReflectionProperty(ChunkCache::class, "compressor");
+        static $_this = new ReflectionClass(self::class);
 
-	/** @var DimensionIds::* */
-	public int $dimension_id;
+        $instance = $_this->newInstanceWithoutConstructor();
+        $instance->dimension_id = $dimension_id;
+        $_world->setValue($instance, $_world->getValue($parent));
+        $_compressor->setValue($instance, $_compressor->getValue($parent));
+        $_caches->setValue($instance, $_caches->getValue($parent));
+        $_hits->setValue($instance, $_hits->getValue($parent));
+        $_misses->setValue($instance, $_misses->getValue($parent));
 
-	public function request(int $chunkX, int $chunkZ) : CompressBatchPromise|string{
-		static $_world = new ReflectionProperty(ChunkCache::class, "world");
-		static $_caches = new ReflectionProperty(ChunkCache::class, "caches");
-		static $_hits = new ReflectionProperty(ChunkCache::class, "hits");
-		static $_misses = new ReflectionProperty(ChunkCache::class, "misses");
-		static $_compressor = new ReflectionProperty(ChunkCache::class, "compressor");
+        return $instance;
+    }
 
-		$world = $_world->getValue($this);
-		$caches = $_caches->getValue($this);
-		$hits = $_hits->getValue($this);
-		$misses = $_misses->getValue($this);
-		$compressor = $_compressor->getValue($this);
-		$chunkHash = World::chunkHash($chunkX, $chunkZ);
-		if(isset($caches[$chunkHash])){
-			++$hits;
-			$_hits->setValue($this, $hits);
-			return $caches[$chunkHash];
-		}
+    /** @var DimensionIds::* */
+    public int $dimension_id;
 
-		$world->registerChunkListener($this, $chunkX, $chunkZ);
-		$chunk = $world->getChunk($chunkX, $chunkZ);
-		if($chunk === null){
-			throw new \InvalidArgumentException("Cannot request an unloaded chunk");
-		}
+    public function request(int $chunkX, int $chunkZ) : CompressBatchPromise {
+        static $_world = new ReflectionProperty(ChunkCache::class, "world");
+        static $_caches = new ReflectionProperty(ChunkCache::class, "caches");
+        static $_hits = new ReflectionProperty(ChunkCache::class, "hits");
+        static $_misses = new ReflectionProperty(ChunkCache::class, "misses");
+        static $_compressor = new ReflectionProperty(ChunkCache::class, "compressor");
 
-		++$misses;
-		$_misses->setValue($this, $misses);
+        $world = $_world->getValue($this);
+        $caches = $_caches->getValue($this);
+        $hits = $_hits->getValue($this);
+        $misses = $_misses->getValue($this);
+        $compressor = $_compressor->getValue($this);
 
-		$world->timings->syncChunkSendPrepare->startTiming();
-		try{
-			$promise = new CompressBatchPromise();
-			$world->getServer()->getAsyncPool()->submitTask(
-				new ChunkRequestTask(
-					$chunkX,
-					$chunkZ,
-					$this->dimension_id,
-					$chunk,
-					$promise,
-					$compressor
-				)
-			);
+        $chunkHash = World::chunkHash($chunkX, $chunkZ);
 
-			$caches[$chunkHash] = $promise;
-			$_caches->setValue($this, $caches);
-			$promise->onResolve(function(CompressBatchPromise $promise) use ($chunkHash, $_caches) : void{
-				//the promise may have been discarded or replaced if the chunk was unloaded or modified in the meantime
-				$caches = $_caches->getValue($this);
-				if(($caches[$chunkHash] ?? null) === $promise){
-					$caches[$chunkHash] = $promise->getResult();
-					$_caches->setValue($this, $caches);
-				}
-			});
+        if (isset($caches[$chunkHash])) {
+            ++$hits;
+            $_hits->setValue($this, $hits);
+            return $caches[$chunkHash];
+        }
 
-			return $promise;
-		}finally{
-			$world->timings->syncChunkSendPrepare->stopTiming();
-		}
-	}
+        $world->registerChunkListener($this, $chunkX, $chunkZ);
+        $chunk = $world->getChunk($chunkX, $chunkZ);
+
+        if ($chunk === null) {
+            throw new \InvalidArgumentException("Cannot request an unloaded chunk");
+        }
+
+        ++$misses;
+        $_misses->setValue($this, $misses);
+
+        $world->timings->syncChunkSendPrepare->startTiming();
+        try {
+            $promise = new CompressBatchPromise();
+            $world->getServer()->getAsyncPool()->submitTask(
+                new ChunkRequestTask(
+                    $chunkX,
+                    $chunkZ,
+                    $this->dimension_id,
+                    $chunk,
+                    $promise
+                )
+            );
+
+            $caches[$chunkHash] = $promise;
+            $_caches->setValue($this, $caches);
+
+            $promise->onResolve(function(CompressBatchPromise $promise) use ($chunkHash, $_caches) : void {
+                // the promise may have been discarded or replaced if the chunk was unloaded or modified in the meantime
+                $caches = $_caches->getValue($this);
+                if (($caches[$chunkHash] ?? null) === $promise) {
+                    $caches[$chunkHash] = $promise->getResult();
+                    $_caches->setValue($this, $caches);
+                }
+            });
+
+            return $promise;
+        } finally {
+            $world->timings->syncChunkSendPrepare->stopTiming();
+        }
+    }
 }
